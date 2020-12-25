30a2c3267c8ad721db3540a2a603f216554610a1
binyao2020 <71888431+binyao2020@users.noreply.github.com>
2020-12-24 19:02:13 -0500

    Add files via upload

---
+++ exact.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 24 18:54:00 2020
+"""
+
+"""
+Input: an instance of the airplane refueling problem
+Output: scheduling solution and objective by Li, et al 's exact algorithm 
+"""
+
+def exact(instance)
+    schedule = []
+    obj = []
+    
+    return schedule,obj
+
+
