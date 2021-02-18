53d71e87566f62ebb4841fb6b149a90e964a28ca
binyao2020 <71888431+binyao2020@users.noreply.github.com>
2021-02-18 16:17:03 -0500

    Update ENSEMBLE.py

--- ENSEMBLE.py
+++ ENSEMBLE.py
@@ -46,6 +46,36 @@ def get_structured_instance(instance, epsilon):
     N = [len(class_list[i]) for i in range(len(class_list))]
     return N, class_list  
 
+def generate_vectors(instance,epsilon,bound=inf): 
+    """
+    generate all state vectors precedes the biggest vector N, and bounded by bound (set to infinite by default)
+    return vec_list
+    """
+    # generate all vectors precedes N
+    L = len(N)
+    vec_list = []
+    cr_list = np.array([(1+epsilon)**i for i in range(1,L+1)]) # list of consumption rate for each class
+    bound_list = np.array([N[i] for i in range(L)]) # the bound for number of planes for each class
+    vec_list = list(range(bound_list[0]+1)) # initialization
+    for i in range(1,L):
+        l = list(range(bound_list[i]+1))
+        vec_list = list(product(vec_list,l))
+        for j in range(len(vec_list)):
+            if i == 1:
+                tp1 = (vec_list[j][0],)
+            else:
+                tp1 = vec_list[j][0] 
+            tp2 = (vec_list[j][1],)
+            vec_list[j] = tp1+tp2 
+    count = 0
+    # remove vectors that exceeds the bound
+    if bound < inf:
+        for j in range(len(vec_list)):
+            if np.dot(vec_list[count],cr_list) > bound:
+                vec_list.pop(count)
+            else:
+                count += 1
+    return vec_list
 
 epsilon = sqrt(2)-1
 instance = generate_instance(25,1,2)
